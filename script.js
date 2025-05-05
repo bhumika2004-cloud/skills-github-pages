a5917720fcc776759a684fa9c7d0c9cee62fa6b6
Bhumika Khadka <khadkabhumika09@gmail.com>
2025-05-05 15:45:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeInfo() {
+  const name = document.getElementById("name");
+  const bio = document.getElementById("bio");
+
+  name.textContent = "Just Me, Bhumika!";
+  bio.textContent = "I am gettting to know about my field and want to pursue my career in either data or AI field.With time I am hoping to develop more skills.";
+}
